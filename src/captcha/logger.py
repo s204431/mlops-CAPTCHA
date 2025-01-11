a8ae8b652425574628e6b4a0908ebb8dfb2a3d80
Petrb <petrboska@gmail.com>
2025-01-11 15:17:03 +0100

    logger cleanup

--- src/captcha/logger.py
+++ src/captcha/logger.py
@@ -1,23 +1,16 @@
-# captcha/logger.py
-
 from loguru import logger
 import sys
-from pathlib import Path
-import pytorch_lightning as pl
-# Ensure the logs directory exists
-Path("outputs").mkdir(parents=True, exist_ok=True)
 
-# Remove the default Loguru handler to prevent duplicate logs
-logger.remove()
+# Remove the default logger
+logger.remove() 
 
-# Add a console handler with a specific format and INFO level
+# Add a new logger with a custom format
 logger.add(
     sys.stdout, 
     format="{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}", 
     level="INFO",
     colorize=True
 )
-
 
 # Export the logger for use in other modules
 __all__ = ["logger"]
