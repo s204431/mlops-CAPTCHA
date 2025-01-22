de87ee9b4447d0dbb2daeba5612c39ce46b2f17a
s204431 <s204431@student.dtu.dk>
2025-01-22 10:45:36 +0100

    Added system metrics to backend

--- src/captcha/bentoml_service.py
+++ src/captcha/bentoml_service.py
@@ -6,6 +6,13 @@ from onnxruntime import InferenceSession
 from google.cloud import storage
 import datetime
 import json
+from prometheus_client import Counter, Histogram, Summary
+from fastapi import HTTPException
+
+error_counter = Counter("prediction_error", "Number of prediction errors")
+request_counter = Counter("prediction_requests", "Number of prediction requests")
+request_latency = Histogram("prediction_latency_seconds", "Prediction latency in seconds")
+image_size_summary = Summary("image_size_summary", "Image size summary")
 
 
 @bentoml.service(workers=2)
@@ -23,12 +30,19 @@ class CaptchaClassifierService:
     )
     def predict(self, image: np.ndarray) -> np.ndarray:
         """Predict the class of the input image."""
-        output = self.model.run(None, {"input": image.astype(np.float32)})
-        class_names = np.array(
-            ["2", "3", "4", "5", "6", "7", "8", "a", "b", "c", "d", "e", "f", "g", "m", "n", "p", "w", "x", "y"]
-        )
-        self.save_prediction_to_gcp(image, output[0][0].tolist(), class_names[np.argmax(output[0][0])])
-        return output[0]
+        request_counter.inc()
+        with request_latency.time():
+            try:
+                image_size_summary.observe(image.shape[2] * image.shape[3])
+                output = self.model.run(None, {"input": image.astype(np.float32)})
+                class_names = np.array(
+                    ["2", "3", "4", "5", "6", "7", "8", "a", "b", "c", "d", "e", "f", "g", "m", "n", "p", "w", "x", "y"]
+                )
+                self.save_prediction_to_gcp(image, output[0][0].tolist(), class_names[np.argmax(output[0][0])])
+                return output[0]
+            except Exception as e:
+                error_counter.inc()
+                raise HTTPException(status_code=500, detail=str(e)) from e
 
     def save_prediction_to_gcp(self, image: np.ndarray, outputs: list[float], prediction: str) -> None:
         """Save the prediction results to GCP bucket."""
